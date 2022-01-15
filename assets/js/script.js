296cff95d16e16f3611220cb5ab725b8564ba853
Remnant <georgesimon4801@gmail.com>
2022-01-15 14:40:27 -0500

    user prompt error

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,40 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
+
+  // Prompt for pasword lengh
+  var pwdLgth = Number(window.prompt("Please enter desired length of password", "Select a number between 8 - 128" ));
+ 
+  if (pwdLgth > 8 || pwdLgth < 128) {
+    console.log(pwdLgth);
+  } else {
+    Number(window.prompt("Please enter desired length of password", "Select a number between 8 - 128" ));
+
+    }
+      
+
+  console.log(String.fromCharCode(97));
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
+
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);{
+  // add user prompt for password
+
+  // add password length function 8-128 characters
+
+  // add characters type prompt
+
+  //at least one character tyoe is required to be selected
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
